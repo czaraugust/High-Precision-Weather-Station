2a3092bc5bc2a74b5f9e0f58b4a99c4c8c295f92
czaraugust <cesaraugusto@ic.ufal.br>
2018-01-05 18:24:40 -0300

    Starting merge of sensors

--- Arduino codes/BMP280/src/main.cpp
+++ Arduino codes/BMP280/src/main.cpp
@@ -6,22 +6,68 @@
 
  ***************************************************************************/
 
+
+/************************
+       BIBLIOTECAS
+************************/
+
+
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <BH1750.h>
 
+
+/************************
+       DEFINIÇÕES
+************************/
+
+
+#define Hall sensor 16  //Definição do Pino do anemômetro
+#define SEALEVELPRESSURE_HPA (1013.25) //Presssão ao nível do mar. Usado para o cálculo de Altitude
+
+
+/********************************************
+       INICIALIZAÇÃO DO SENSORES
+*********************************************/
+Adafruit_BME280 bme; // I2C
 BH1750 lightMeter;
 
-
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-Adafruit_BME280 bme; // I2C
-
-
 unsigned long delayTime;
+
+/********************************************
+       DEFINIÇÃO DE CONSTANTES DO ANEMÔMETRO
+*********************************************/
+
+const int anemoPin = 12;
+const float pi = 3.14159265;           // Numero pi
+int period = 5000;               // Tempo de medida(miliseconds)
+int delaytime = 5000;             // Time between samples (miliseconds)
+int radius = 147;      // Raio do anemometro(mm)
+long delayWhile = 0;
+//  Variable definitions
+unsigned int Sample = 0;   // Sample number
+unsigned int counter = 0; // magnet counter for sensor
+unsigned int RPM = 0;          // Revolutions per minute
+float speedwind = 0;         // Wind speed (m/s)
+float windspeed = 0;           // Wind speed (km/h)
+long readTime = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
 void printValues();
+
 void setup() {
     Serial.begin(9600);
     Serial.println(F("BME280 test"));
@@ -46,6 +92,17 @@ void setup() {
 
     Serial.println();
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 void loop() {
